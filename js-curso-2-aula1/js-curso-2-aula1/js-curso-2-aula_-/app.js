c29e543f6ebd6330f92cfdc17e6e4e33943adc0b
SPSsilva <saulosilva775@gmail.com>
2024-01-30 21:47:10 -0300

    30/01

---
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,24 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Hackeando bicicleta';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um aro de 1 a 10';
+// let numeroSecreto = 9;
+
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+// }
+
+// exibirTextoNaTela('h1', 'Hackeando bicicleta!');
+// exibirTextoNaTela('p', 'Escolha um aro de 1 a 10');
+
+// function verificarChute() {
+//     let chute = document.querySelector('input').value
+//     console.log(numeroSecreto == numeroSecreto);
+// }
+
+// function numeroAleatorio() {
+//     return parseInt(Math.random() * 10 + 1);
+// }
+
